489d83f66e943ff62f00936aa21f7f5eb292cf1d
Andi Xu <andixu@umich.edu>
2025-11-27 12:02:15 -0800

    Add locomotion

--- OmniGibson/omnigibson/robots/robot.py
+++ OmniGibson/omnigibson/robots/robot.py
@@ -37,6 +37,7 @@ from omnigibson.controllers import (
     ManipulationController,
     MultiFingerGripperController,
     OperationalSpaceController,
+    LocomotionController
 )
 from omnigibson.controllers.joint_controller import JointController
 from omnigibson.objects.object_base import BaseObject
@@ -83,6 +84,9 @@ m.AG_DEFAULT_GRASP_POINT_Z_PROP = 0.4
 m.CONSTRAINT_VIOLATION_THRESHOLD = 0.1
 m.GRASP_WINDOW = 3.0  # grasp window in seconds
 m.RELEASE_WINDOW = 1 / 30.0  # release window in seconds
+
+# ---Locomotion---
+
 
 AG_MODES = {
     "physical",
@@ -1353,7 +1357,13 @@ class Robot(USDObject, BaseObject, GymObservable):
                         self._controllers["gripper_{}".format(arm)], GripperController
                     ), "Gripper {} controller must be a GripperController!".format(arm)
 
-        pass
+        if self.is_locomotion:
+            # If we have a base controller, make sure it is a locomotion controller
+            if "base" in self._controllers:
+                assert isinstance(
+                    self._controllers["base"], LocomotionController
+                ), "Base controller must be a LocomotionController!"
+
 
     def get_obs(self):
         """
@@ -1394,7 +1404,17 @@ class Robot(USDObject, BaseObject, GymObservable):
             dict: empty dictionary, a placeholder for additional info
         """
         proprio_dict = self._get_proprioception_dict()
-        return th.cat([proprio_dict[obs] for obs in self._proprio_obs]), {}
+        dic = th.cat([proprio_dict[obs] for obs in self._proprio_obs]), {}
+        if self.is_locomotion:
+            joint_positions = dic["joint_qpos"]
+            joint_velocities = dic["joint_qvel"]
+
+            # Add base info
+            dic["base_qpos"] = joint_positions[self.base_control_idx]
+            dic["base_qpos_sin"] = th.sin(joint_positions[self.base_control_idx])
+            dic["base_qpos_cos"] = th.cos(joint_positions[self.base_control_idx])
+            dic["base_qvel"] = joint_velocities[self.base_control_idx]
+        return dic
 
     def _get_proprioception_dict(self):
         """
@@ -2029,8 +2049,8 @@ class Robot(USDObject, BaseObject, GymObservable):
         Returns:
             list of str: Default proprioception observations to use
         """
+        obs_keys=[]
         if self.is_manipulation:
-            obs_keys = super().default_proprio_obs
             for arm in self.arm_names:
                 obs_keys += [
                     "arm_{}_qpos_sin".format(arm),
@@ -2041,7 +2061,9 @@ class Robot(USDObject, BaseObject, GymObservable):
                     "grasp_{}".format(arm),
                 ]
             return obs_keys
-        return []
+        if self.is_locomotion:
+            obs_keys += ["base_qpos_sin", "base_qpos_cos", "robot_lin_vel", "robot_ang_vel"]
+        return obs_keys
 
     @property
     def model_name(self):
@@ -2300,6 +2322,12 @@ class Robot(USDObject, BaseObject, GymObservable):
                     gripper_joint_configs[arm]["name"]: gripper_joint_configs[arm],
                     gripper_null_configs[arm]["name"]: gripper_null_configs[arm],
                 }
+        if self.is_locomotion:
+             # Add supported base controllers
+            cfg["base"] = {
+                self._default_base_joint_controller_config["name"]: self._default_base_joint_controller_config,
+                self._default_base_null_joint_controller_config["name"]: self._default_base_null_joint_controller_config,
+            }
         return cfg
 
     def _get_assisted_grasp_joint_type(self, ag_obj, ag_link):
@@ -2429,7 +2457,9 @@ class Robot(USDObject, BaseObject, GymObservable):
             for arm in self.arm_names:
                 controllers["arm_{}".format(arm)] = "JointController"
                 controllers["gripper_{}".format(arm)] = "JointController"
-
+        if self.is_locomotion:
+            # For best generalizability use, joint controller as default
+            controllers["base"] = "JointController"
         return controllers
     
     @property
@@ -3353,3 +3383,172 @@ class Robot(USDObject, BaseObject, GymObservable):
         return {arm: th.tensor([0, 0, 0, 1]) for arm in self.arm_names}
 
 
+    @property
+    def _raw_controller_order(self):
+        assert self.is_locomotion
+        # By default, only base is supported
+        return ["base"]
+
+  
+    @property
+    def _default_base_joint_controller_config(self):
+        """
+        Returns:
+            dict: Default base joint controller config to control this robot's base. Uses velocity
+                control by default.
+        """
+        assert self.is_locomotion
+        return {
+            "name": "JointController",
+            "control_freq": self._control_freq,
+            "motor_type": "velocity",
+            "control_limits": self.control_limits,
+            "dof_idx": self.base_control_idx,
+            "command_output_limits": "default",
+            "use_delta_commands": False,
+        }
+
+    @property
+    def _default_base_null_joint_controller_config(self):
+        """
+        Returns:
+            dict: Default null joint controller config to control this robot's base i.e. dummy controller
+        """
+        assert self.is_locomotion
+        return {
+            "name": "NullJointController",
+            "control_freq": self._control_freq,
+            "motor_type": "velocity",
+            "control_limits": self.control_limits,
+            "dof_idx": self.base_control_idx,
+            "default_goal": th.zeros(len(self.base_control_idx)),
+            "use_impedances": False,
+        }
+
+    
+
+    def move_by(self, delta):
+        """
+        Move robot base without physics simulation
+
+        Args:
+            delta (float):float], (x,y,z) cartesian delta base position
+        """
+        assert self.is_locomotion
+        new_pos = th.tensor(delta) + self.get_position_orientation()[0]
+        self.set_position_orientation(position=new_pos)
+
+    def move_forward(self, delta=0.05):
+        """
+        Move robot base forward without physics simulation
+
+        Args:
+            delta (float): delta base position forward
+        """
+        assert self.is_locomotion
+        self.move_by(T.quat2mat(self.get_position_orientation()[1]).dot(th.tensor([delta, 0, 0])))
+
+    def move_backward(self, delta=0.05):
+        """
+        Move robot base backward without physics simulation
+
+        Args:
+            delta (float): delta base position backward
+        """
+        assert self.is_locomotion
+        self.move_by(T.quat2mat(self.get_position_orientation()[1]).dot(th.tensor([-delta, 0, 0])))
+
+    def move_left(self, delta=0.05):
+        """
+        Move robot base left without physics simulation
+
+        Args:
+            delta (float): delta base position left
+        """
+        assert self.is_locomotion
+        self.move_by(T.quat2mat(self.get_position_orientation()[1]).dot(th.tensor([0, -delta, 0])))
+
+    def move_right(self, delta=0.05):
+        """
+        Move robot base right without physics simulation
+
+        Args:
+            delta (float): delta base position right
+        """
+        assert self.is_locomotion
+        self.move_by(T.quat2mat(self.get_position_orientation()[1]).dot(th.tensor([0, delta, 0])))
+
+    def turn_left(self, delta=0.03):
+        """
+        Rotate robot base left without physics simulation
+
+        Args:
+            delta (float): delta angle to rotate the base left
+        """
+        assert self.is_locomotion
+        quat = self.get_position_orientation()[1]
+        quat = T.quat_multiply((T.euler2quat(-delta, 0, 0)), quat)
+        self.set_position_orientation(orientation=quat)
+
+    def turn_right(self, delta=0.03):
+        """
+        Rotate robot base right without physics simulation
+
+        Args:
+            delta (float): angle to rotate the base right
+        """
+        assert self.is_locomotion
+        quat = self.get_position_orientation()[1]
+        quat = T.quat_multiply((T.euler2quat(delta, 0, 0)), quat)
+        self.set_position_orientation(orientation=quat)
+
+    @cached_property
+    def non_floor_touching_base_links(self):
+        assert self.is_locomotion
+        return [self.links[name] for name in self.non_floor_touching_base_link_names]
+
+    @cached_property
+    def non_floor_touching_base_link_names(self):
+        assert self.is_locomotion
+        return [self.base_footprint_link_name]
+
+    @cached_property
+    def floor_touching_base_links(self):
+        assert self.is_locomotion
+        return [self.links[name] for name in self.floor_touching_base_link_names]
+
+    @cached_property
+    def floor_touching_base_link_names(self):
+        assert self.is_locomotion
+        raise NotImplementedError
+
+    @property
+    def base_action_idx(self):
+        assert self.is_locomotion
+        controller_idx = self.controller_order.index("base")
+        action_start_idx = sum([self.controllers[self.controller_order[i]].command_dim for i in range(controller_idx)])
+        return th.arange(action_start_idx, action_start_idx + self.controllers["base"].command_dim)
+
+    @property
+    # @abstractmethod
+    def base_joint_names(self):
+        """
+        Returns:
+            list: Array of joint names corresponding to this robot's base joints (e.g.: wheels).
+
+                Note: the ordering within the list is assumed to be intentional, and is
+                directly used to define the set of corresponding control idxs.
+        """
+        assert self.is_locomotion
+        raise NotImplementedError
+
+    @cached_property
+    def base_control_idx(self):
+        """
+        Returns:
+            n-array: Indices in low-level control vector corresponding to base joints.
+        """
+        assert self.is_locomotion
+        return th.tensor([list(self.joints.keys()).index(name) for name in self.base_joint_names])
+
+  
